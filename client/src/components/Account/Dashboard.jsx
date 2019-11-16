aceae3f7abd00852588ea1c83fc8adb7ef094331
Sadman <hello@sadmansh.com>
2019-11-16 21:05:58 +0600

    Add ant form for creating listings

--- client/src/components/Account/Dashboard.jsx
+++ client/src/components/Account/Dashboard.jsx
@@ -2,9 +2,12 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {Redirect} from 'react-router-dom'
 import {auth, search} from '../../actions'
-import {Layout, Row, Col, Card} from 'antd'
+import {Layout, Row, Col, Card, Input, Select, Button} from 'antd'
 import 'antd/dist/antd.css'
 
+
+const {TextArea} = Input
+const {Option} = Select
 
 class Dashboard extends Component {
 
@@ -20,15 +23,48 @@ class Dashboard extends Component {
 			<div id="dashboard" className="container">
 				<h1>Welcome to your dashboard, {this.props.user.name}</h1>
 				<a onClick={this.props.logout}>Logout</a>
-				{this.props.services.map((service, id) => (
-					<Card title={service.title} extra={<a href="#">Edit</a>} key={id} style={{ marginTop: 16 }}>
-						<Row gutter={16}>
-							<Col span={24}>
-								<p>{service.description}</p>
-							</Col>
-						</Row>
-					</Card>
-				))}
+				<div id="create-listing">
+					<h2>Create New Listing</h2>
+					<Row gutter={16}>
+						<Col span={24}>
+							<Input addonBefore="Title" type="text" style={{ marginBottom: 16 }} />
+						</Col>
+						<Col span={24}>
+							<TextArea placeholder="Description" autoSize={{ minRows: 2, maxRows: 6 }} style={{ marginBottom: 16 }} />
+						</Col>
+						<Col span={12}>
+							<Input addonBefore="Keywords" type="text" style={{ marginBottom: 16 }} />
+						</Col>
+						<Col span={12}>
+							<Select showSearch style={{ width: '100%', marginBottom: 16 }} placeholder="Select a category" optionFilterProp="children">
+								<Option value="jack">Jack</Option>
+								<Option value="lucy">Lucy</Option>
+								<Option value="tom">Tom</Option>
+							</Select>
+						</Col>
+						<Col span={12}>
+							<Input addonBefore="Phone" type="text" style={{ marginBottom: 16 }} />
+						</Col>
+						<Col span={12}>
+							<Input addonBefore="Location" type="text" style={{ marginBottom: 16 }} />
+						</Col>
+						<Col span={24} style={{ marginBottom: 16 }}>
+							<Button type="primary" htmlType="submit">Create Listing</Button>
+						</Col>
+					</Row>
+				</div>
+				<div id="all-listings">
+					<h2>Your listings</h2>
+					{this.props.services.map((service, id) => (
+						<Card title={service.title} extra={<a href="#">Edit</a>} key={id} style={{ marginTop: 16 }}>
+							<Row gutter={16}>
+								<Col span={24}>
+									<p>{service.description}</p>
+								</Col>
+							</Row>
+						</Card>
+					))}
+				</div>
 			</div>
 		)
 	}
